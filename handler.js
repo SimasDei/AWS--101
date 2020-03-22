e39930e714876720da83545cabacd19d2e58086b
Simas <simas@simas-dei.com>
2020-03-22 11:22:44 +0200

    generate package.lock, amend serverless config

--- handler.js
+++ handler.js
@@ -1,18 +1,37 @@
 'use strict';
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v4');
 
-module.exports.hello = async event => {
+const db = new AWS.DynamoDB.documentClient();
+
+const postsTable = process.env.POST_TABLE;
+
+const response = (statusCode, message) => {
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    statusCode,
+    body: JSON.stringify(message),
+  };
+};
+
+module.exports.createPost = (event, context, callback) => {
+  const reqBody = JSON.parse(event.body);
+
+  const post = {
+    id: uuid(),
+    createdAt: new Date().toISOString(),
+    userId: 1,
+    title: reqBody.title,
+    body: reqBody.body,
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return db
+    .put({
+      TableName: postsTable,
+      Item: post,
+    })
+    .promise()
+    .then(() => {
+      callback(null, response(201, post));
+    })
+    .catch(err => response(null, response(err.statusCode, err)));
 };
