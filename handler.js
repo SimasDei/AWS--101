70634e960cf78a552f582669fdeed521540722d7
Simas <simas@simas-dei.com>
2020-03-22 12:03:35 +0200

    add update and delete post Endpoints

--- handler.js
+++ handler.js
@@ -47,9 +47,7 @@ module.exports.getAllPosts = (event, context, callback) => {
       TableName: postsTable,
     })
     .promise()
-    .then(res => {
-      callback(null, res.Items.sort(sortByDate));
-    })
+    .then(res => callback(null, res.Items.sort(sortByDate)))
     .catch(err => callback(null, response(err.statusCode, err)));
 };
 
@@ -62,9 +60,7 @@ module.exports.getPosts = (event, context, callback) => {
   return db
     .scan(params)
     .promise()
-    .then(res => {
-      callback(null, res.Items.sort(sortByDate));
-    })
+    .then(res => callback(null, res.Items.sort(sortByDate)))
     .catch(err => callback(null, response(err.statusCode, err)));
 };
 
@@ -84,5 +80,44 @@ module.exports.getPost = (event, context, callback) => {
       if (res.Item) return callback(null, res.Item);
       return callback(null, response(404, { error: 'Post not found' }));
     })
+    .catch(err => callback(null, response(err.statusCode, err)));
+};
+
+module.exports.updatePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const { paramName, paramValue } = JSON.parse(event.body);
+  const params = {
+    Key: {
+      id,
+    },
+    TableName: postsTable,
+    ConditionExpression: 'attribute_exists(id)',
+    UpdateExpressiont: `set ${paramName} = :v`,
+    ExpressiontAttributeValues: {
+      ':v': paramValue,
+    },
+    ReturnValue: 'All_NEW',
+  };
+
+  return db
+    .update(params)
+    .promise()
+    .then(res => callback(null, response(200, res)))
+    .catch(err => callback(null, response(err.statusCode, err)));
+};
+
+module.exports.deletePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const params = {
+    Key: {
+      id,
+    },
+    TableName: postsTable,
+  };
+
+  return db
+    .delete(params)
+    .promise()
+    .then(() => callback(null, response(200, { message: 'Successfully deleted' })))
     .catch(err => callback(null, response(err.statusCode, err)));
 };
